746874f95a4ee14c79def84d52b7ace9027a7798
Jaroslav Boudný <jboudny0@gmail.com>
2015-12-26 20:53:16 +0100

    Added a method to create a perspective projection Matrix4

--- src/main/java/cz/trigon/bicepsrendererapi/gl/impl/matrices/Matrix4.java
+++ src/main/java/cz/trigon/bicepsrendererapi/gl/impl/matrices/Matrix4.java
@@ -13,6 +13,25 @@ public class Matrix4 implements IMatrix {
         this.data = ByteBuffer.allocateDirect(4 * 4 * 4);
     }
 
+    public static Matrix4 makeProjMatrix(float fov, float aspect, float near, float far) {
+        Matrix4 mat = new Matrix4();
+        FloatBuffer data = mat.getData().asFloatBuffer();
+
+        float uh = (float) (1f / Math.tan(fov / 2f));
+        float uw = -uh / aspect;
+
+        float[] dataArray = new float[]{
+                uw, 0, 0, 0,
+                0, uh, 0, 0,
+                0, 0, far/(far-near), 1,
+                0, 0, -(far*near)/(far-near), 0
+        };
+
+        data.put(dataArray);
+
+        return mat;
+    }
+
     public static Matrix4 makeOrthoMatrix(float l, float r, float b, float t, float n, float f) {
         Matrix4 mat = new Matrix4();
         FloatBuffer data = mat.getData().asFloatBuffer();
