1a860adf1f1f70c2b2498af439169b5ee6fb6fca
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2016-01-01 22:04:38 +0100

    Added Vector3, made Vectors immutable

--- src/main/java/cz/trigon/bicepsrendererapi/util/Vector3.java
+++ src/main/java/cz/trigon/bicepsrendererapi/util/Vector3.java
@@ -1,22 +1,25 @@
 package cz.trigon.bicepsrendererapi.util;
 
-public class Vector2 {
+public class Vector3 {
     protected float x;
     protected float y;
-    public Vector2() {
-        this(0, 0);
+    protected float z;
+
+    public Vector3() {
+        this(0, 0, 0);
     }
 
-    public Vector2(Vector2 toCopy) {
-        this(toCopy.x(), toCopy.y());
+    public Vector3(Vector3 toCopy) {
+        this(toCopy.x(), toCopy.y(), toCopy.z());
     }
 
-    public Vector2(float x, float y) {
+    public Vector3(float x, float y, float z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
-    public float getDistance(Vector2 other) {
+    public float getDistance(Vector3 other) {
         if (other.equals(this))
             return 0;
 
@@ -26,7 +29,8 @@ public class Vector2 {
         if (other.y() == this.y)
             return Math.abs(other.x - this.x);
 
-        return (float) Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(this.y - other.y, 2));
+        return (float) Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(this.y - other.y, 2)
+                + Math.pow(other.z - this.z, 2));
     }
 
     public float x() {
@@ -37,11 +41,15 @@ public class Vector2 {
         return this.y;
     }
 
+    public float z() {
+        return this.z;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (o instanceof Vector2) {
-            Vector2 v = (Vector2) o;
-            return (v.x == this.x && v.y == this.y);
+        if (o instanceof Vector3) {
+            Vector3 v = (Vector3) o;
+            return (v.x == this.x && v.y == this.y && v.z == this.z);
         }
 
         return false;
@@ -54,6 +62,6 @@ public class Vector2 {
 
     @Override
     public String toString() {
-        return this.x + ":" + this.y();
+        return this.x + ";" + this.y + ";" + this.z;
     }
 }
