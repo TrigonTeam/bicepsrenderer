1615a5b1c77671a68278b62c0be8a30f09731bb6
Jaroslav Boudný <jboudny0@gmail.com>
2016-01-05 15:34:57 +0100

    Cleanup

--- src/main/java/cz/trigon/bicepsrendererapi/gl/render/ImmediateRenderer.java
+++ src/main/java/cz/trigon/bicepsrendererapi/gl/render/ImmediateRenderer.java
@@ -140,7 +140,7 @@ public class ImmediateRenderer implements IImmediateRenderer {
     }
 
     public static float packColor(int r, int g, int b, int a) {
-        return Float.intBitsToFloat(((a & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8)  | ((b & 0xFF) << 0));
+        return Float.intBitsToFloat(((a & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8)  | (b & 0xFF));
     }
 
     public static float packColor(int color) {
